fa863fcd5273ee48513ce8be37a394cbbda21961
Emanuele Coricciati <emanuelecoricciati@libero.it>
2024-06-03 00:47:25 +0200

    syscalls added

--- include/syscall.h
+++ include/syscall.h
@@ -59,4 +59,8 @@ __DEAD void enter_new_process(int argc, userptr_t argv, userptr_t env,
 int sys_reboot(int code);
 int sys___time(userptr_t user_seconds, userptr_t user_nanoseconds);
 
+int sys_write(int fd, userptr_t buf_ptr, size_t size);
+int sys_read(int fd, userptr_t buf_ptr, size_t size);
+void sys__exit(int status);
+
 #endif /* _SYSCALL_H_ */
